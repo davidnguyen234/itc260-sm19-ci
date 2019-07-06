636e581ceef2675d59f242ca8f75c9ae8610923d
David Nguyen <davidnguyen234@gmail.com>
2019-07-06 14:36:00 -0700

    Adds feedback and slug to Create()
    Now when a new item is added, the slug is passed to the view and we see the new item, with feedback!

--- sm19/application/controllers/News.php
+++ sm19/application/controllers/News.php
@@ -31,9 +31,9 @@ class News extends CI_Controller {
 
             $data['title'] = $data['news_item']['title'];
 
-            $this->load->view('templates/header', $data);
+            //$this->load->view('templates/header', $data);
             $this->load->view('news/view', $data);
-            $this->load->view('templates/footer', $data);
+            //$this->load->view('templates/footer', $data);
     }
 
 
@@ -49,17 +49,25 @@ class News extends CI_Controller {
 
         if ($this->form_validation->run() === FALSE)
         {
-            $this->load->view('templates/header', $data);
+            //$this->load->view('templates/header', $data);
             $this->load->view('news/create', $data);
-            $this->load->view('templates/footer', $data);
+            //$this->load->view('templates/footer', $data);
 
         }
         else
         {
-            $this->news_model->set_news();
-            $this->load->view('templates/header', $data);
-            $this->load->view('news/success', $data);
-            $this->load->view('templates/footer', $data);
+            //$this->news_model->set_news();
+            //$this->load->view('templates/header', $data);
+            //$this->load->view('news/success', $data);
+            //$this->load->view('templates/footer', $data);
+                $slug = $this->news_model->set_news();
+                if($slug !==false){//slug sent
+                        feedback('Data entered successfully!','info');
+                        redirect('news/view/' . $slug);
+                }else{//error
+                        feedback('Data NOT entered!','error');
+                        redirect('news/create');
+                }
         }
     }
 
--- sm19/application/models/News_model.php
+++ sm19/application/models/News_model.php
@@ -33,9 +33,13 @@ class News_model extends CI_Model {
                 'text' => $this->input->post('text')
             );
 
-            return $this->db->insert('sm19_news', $data);
+            //return $this->db->insert('sm19_news', $data);
+
+            if($this->db->insert('sm19_news', $data))
+            {//return slug
+                return $slug;
+            }else{//return false
+                return false;
+            }
         }
-
-
-
 }
